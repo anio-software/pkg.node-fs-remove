cf4695c3a14051718d186204e2e95d518c72e92c
eXory2024 <exory@anio.software>
2024-04-03 20:20:31 +0200

    Provide types.

---
+++ src/index.d.ts
@@ -0,0 +1,17 @@
+/**
+ * @brief Asynchronously emove a path of any type.
+ * @description
+ * Asynchronously removes the entry located at `path`.
+ * This function does not throw if `path` does not exist.
+ * @param path The path to be deleted.
+ */
+export function remove(path : string) : Promise<void>;
+
+/**
+ * @brief Synchronously emove a path of any type.
+ * @description
+ * Synchronously removes the entry located at `path`.
+ * This function does not throw if `path` does not exist.
+ * @param path The path to be deleted.
+ */
+export function removeSync(path : string) : void;
