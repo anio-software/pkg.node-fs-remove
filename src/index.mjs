f3ed0a353a7e83aca29179e0f053724a7390c3bf
eXory2024 <exory@anio.software>
2024-04-03 20:16:27 +0200

    Replace @anio-node-foundation/fs-api with @anio-fs/api.

--- src/index.mjs
+++ src/index.mjs
@@ -1,17 +1,14 @@
-import createFSObject from "@anio-node-foundation/fs-api"
+import fs from "@anio-fs/api"
 
 import sync_impl from "./auto/sync.mjs"
 import async_impl from "./auto/async.mjs"
 
-const async_fs = createFSObject({sync: false})
-const sync_fs = createFSObject({sync: true})
-
 function nodeFsRemove(src, dest) {
-	return async_impl(async_fs, src, dest)
+	return async_impl(fs.async, src, dest)
 }
 
 nodeFsRemove.sync = function(src, dest) {
-	return sync_impl(sync_fs, src, dest)
+	return sync_impl(fs.sync, src, dest)
 }
 
 export default nodeFsRemove
