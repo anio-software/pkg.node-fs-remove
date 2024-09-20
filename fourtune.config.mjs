9e147243f5499c416cfc30f3acc2effb7b0f64ab
eXory2024 <exory@anio.software>
2024-09-20 19:05:52 +0200

    Move from vipen to fourtune.

--- fourtune.config.mjs
+++ fourtune.config.mjs
@@ -1,4 +1,4 @@
-import {generateFromTemplate} from "vipen/autogenerate"
+import {generateFromTemplate} from "fourtune/autogenerate"
 
 const asyncToSync = {
 	"import {unlink, rmdir} from \"@anio-fs/api/async\"": "import {unlink, rmdir} from \"@anio-fs/api/sync\"",
