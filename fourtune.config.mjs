63b53c6227819dd5582652c5011d1ea5f07e60a2
eXory2024 <exory@anio.software>
2024-12-07 23:56:32 +0100

    Update fourtune.config.mjs.

--- fourtune.config.mjs
+++ fourtune.config.mjs
@@ -1,13 +1,6 @@
 export default {
-	realm: "js",
-	type: "package:async/sync",
-
-	target: {
-		function_name: "remove",
-
-		dependencies: {
-			"@anio-fs/scandir": "scandir",
-			"@anio-fs/path-type": "getTypeOfPath"
-		}
+	realm: {
+		name: "js",
+		type: "package"
 	}
 }
